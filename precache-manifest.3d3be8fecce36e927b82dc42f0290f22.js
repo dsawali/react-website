3efbab216b8290979a81a58af331043d9da93419
dsawali <dsawali@sfu.ca>
2019-06-24 22:23:08 -0700

    Updates

---
+++ precache-manifest.3d3be8fecce36e927b82dc42f0290f22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb83ab0f1fb35f9a65bccc8e148c80c5",
+    "url": "/react-website/index.html"
+  },
+  {
+    "revision": "e89c98a07958a15b1112",
+    "url": "/react-website/static/css/main.cb2589ee.chunk.css"
+  },
+  {
+    "revision": "2f87efbfc8b607080f30",
+    "url": "/react-website/static/js/2.65dc48af.chunk.js"
+  },
+  {
+    "revision": "e89c98a07958a15b1112",
+    "url": "/react-website/static/js/main.31801346.chunk.js"
+  },
+  {
+    "revision": "59b994fe6cb40dbf3519",
+    "url": "/react-website/static/js/runtime~main.5bd11a58.js"
+  },
+  {
+    "revision": "d8c1518bdb51a31340caaba056580780",
+    "url": "/react-website/static/media/resume.d8c1518b.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.da8091a5b18c226445aa78a101082d91.js"
+  "/react-website/precache-manifest.3d3be8fecce36e927b82dc42f0290f22.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
